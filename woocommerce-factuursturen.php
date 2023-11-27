440afdd2fe394bc9f1ba056a73ee2ad47fb90f17
Thomas Lysens <thomas@lyssec.com>
2023-11-27 15:09:50 +0300

    Added function for woocommerce_new_order hook to create invoice

--- woocommerce-factuursturen.php
+++ woocommerce-factuursturen.php
@@ -17,6 +17,8 @@
 * License: GNU General Public License v3.0
 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
+use Main\Includes\WCFSNL_MAIN;
+
 // Dependencies
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
@@ -36,5 +38,20 @@ register_deactivation_hook( __FILE__, 'wcfsnl_deactivate' );
 
 // If plugin is active, execute all the rest
 if ( is_plugin_active( 'woocommerce-factuursturen/woocommerce-factuursturen.php' ) ) {
+    add_action( 'woocommerce_new_order', 'wcfsnl_create_invoice');
+    add_action( 'woocommerce_update_order', '' );
+
     require plugin_dir_path( __FILE__ ) . 'includes/wcfsnl-admin-pages.php';
+    require plugin_dir_path( __FILE__ ) . 'includes/wcfsnl-main.php';
+
+    // On new order creation
+    function wcfsnl_create_invoice() {
+        global $woocommerce, $post;
+        $wcfsnl = new WCFSNL_MAIN( get_option('wcfsnl_api_key'), get_option('wcfsnl_api_user') );
+        $order = wc_get_order( $post->ID );
+        $wcfsnl->create_invoice( $order );
+    }
+
+
+
 }
